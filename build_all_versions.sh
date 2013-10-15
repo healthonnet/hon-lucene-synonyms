8ef9fdffbbe0adada9fbf3d72129335c0f55aa9f
Nolan Lawson <nolan.lawson@gmail.com>
2013-10-14 18:07:18 -0700

    allow build_all_versions.sh to detect plugin version automatically

--- build_all_versions.sh
+++ build_all_versions.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-# build all versions of the hon-lucene-synonyms plugin, for the supported Solr versions
 #
-# designed to make it easier to upload these JAR files to S3
+# Build all versions of the hon-lucene-synonyms plugin, for the supported Solr versions
 #
-# writes to directory target/s3
+# Designed to make it easier to upload these JAR files to S3.  Make sure your git workspace is clean before running this!
+#
+# Writes to directory target/s3
+#
 
 SOLR_VERSIONS='3.x 4.0.0 4.1.0 4.3.0';
-PLUGIN_VERSION='1.3.1';
+PLUGIN_VERSION=`cat pom.xml | grep version | head -1 | python -c "import re, sys; print re.search('>(.*?)-solr',sys.stdin.read()).group(1)"`
+
+echo -e "\nPlugin version is ${PLUGIN_VERSION}, building for Solr versions ${SOLR_VERSIONS}...\n"
 
 rm -fr target/s3;
 mkdir target/s3;
 
 for SOLR_VERSION in $SOLR_VERSIONS; do
+    echo -e "\nBuilding for Solr version ${SOLR_VERSION}...\n"
     git checkout solr-$SOLR_VERSION;
     mvn install;
     FULL_VERSION_NAME=$PLUGIN_VERSION-solr-$SOLR_VERSION;
@@ -19,4 +24,5 @@ for SOLR_VERSION in $SOLR_VERSIONS; do
     cp target/hon-lucene-synonyms-$FULL_VERSION_NAME.jar target/s3/$FULL_VERSION_NAME;
 done;
 
-echo "wrote all versions to target/s3"
+echo -e "Wrote all jar files to target/s3.  Here they are:\n"
+find target/s3 -type f
